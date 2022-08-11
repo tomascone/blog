03ad0bb3729bb70ee5a69b20c421b73fe319f040
Tomas <tomascone13@gmail.com>
2022-08-11 10:14:40 -0400

    Maquetacion de layout, home, categorias, post e instalacion de clockwork

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+
+    @include('_partials/_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
+        @if ($posts->count())
+
+            <x-post-grid :posts="$posts" />
+            
+        @else
+            
+            <p class="text-center">No posts yet. Please check back later.</p>
+
+        @endif
+    </main>
+</x-layout>
