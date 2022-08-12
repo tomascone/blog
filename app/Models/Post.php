55c8a07b7a6d23a0b31f821b736aa484beda90c2
tomascone <tomascone13@gmail.com>
2022-08-12 01:55:08 -0400

    Agregar filtrado por author

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,6 +26,12 @@ class Post extends Model
                 ->whereHas('category', fn($query) =>
                 $query->where('slug', $category))
         );
+
+        $query->when($filters['author'] ?? false, fn($query, $author) =>
+            $query
+                ->whereHas('author', fn($query) =>
+                $query->where('username', $author))
+        );
     }
 
     public function category()
