c933cfa05d393640ef41f17e95b77e0690b4a014
tomascone <tomascone13@gmail.com>
2022-08-12 00:29:09 -0400

    Agregar buscador de posts, mover logica de posts de routes a controlador

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,15 @@ class Post extends Model
 
     protected $with = ['category', 'author'];
 
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
+            $query
+                ->where('title', 'like', '%'.$search.'%')
+                ->orwhere('body', 'like', '%'.$search.'%')
+        );
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
