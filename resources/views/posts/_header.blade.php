554fa00bf7855d20c5274902bc4cadeebb97a0af
tomascone <tomascone13@gmail.com>
2022-08-12 01:37:14 -0400

    Crear componente dropdown de categorias, crear y mover vistas de posts a su propia carpeta

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -8,29 +8,7 @@
         <!--  Category -->
         <div class="relative lg:inline-flex items-center bg-gray-100 rounded-xl">
 
-            <x-dropdown>
-                <x-slot name="trigger">      
-                    <button class="bg-transparent py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 flex lg:inline-flex">
-
-                        {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
-
-                        <x-icon class="absolute pointer-events-none" style="right: 12px;" name="down-arrow">
-
-                        </x-icon>
-
-                    </button>
-                </x-slot>
-
-                <x-dropdown-item href="/" :active="request()->routeIs('home')">
-                    All
-                </x-dropdown-item>
-
-                @foreach ($categories as $category)
-                    <x-dropdown-item href="?category={{ $category->slug }}" :active='request()->is("categories/{$category->slug}")'>
-                        {{ ucwords($category->name) }}
-                    </x-dropdown-item>
-                @endforeach
-            </x-dropdown>
+            <x-category-dropdown />
 
         </div>
 
