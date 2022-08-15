aa6c3d5ee5188405fb829dc69c3889dfc5cfc051
tomascone <tomascone13@gmail.com>
2022-08-15 08:34:25 -0400

    Conectar newsletter de footer con mailchimp

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('newsletter', function(){
+    request()->validate([
+        'email' => 'required|email'
+    ]);
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us12'
+    ]);
+
+    try {
+        $response = $mailchimp->lists->addListMember("f7d0090e12", [
+            "email_address" => request('email'),
+            "status" => "subscribed",
+        ]);
+    } catch (\Exception $e) {
+        \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter list'
+        ]);
+    }
+
+    return redirect('/')->with('success', 'You are now signed up to our newsletter');
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
