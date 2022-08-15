ed7af91c1a36592de8bd3b00e8880b342bbafd45
Tomas <tomascone13@gmail.com>
2022-08-15 04:31:01 -0400

    Agregar tabla y factory de comentarios, listar comentarios en post

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -14,8 +14,8 @@ class CategoryFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->unique()->word,
-            'slug' => $this->faker->unique()->slug,
+            'name' => $this->faker->unique()->word(),
+            'slug' => $this->faker->unique()->slug(),
         ];
     }
 }
