b87af440259fd79b69de34ca5640f373479667ac
Tomas <tomascone13@gmail.com>
2022-08-17 05:30:47 -0400

    Agregar thumbnails a posts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,10 @@
 <x-layout>
-    <section class="px-6 py-8">
+    <section class="py-8 max-w-sm mx-auto">
+        <h1 class="text-lg font-bold mb-4">
+            Publish New Post
+        </h1>
         <x-panel class="max-w-sm mx-auto">
-            <form method="POST" action="/admin/posts">
+            <form method="POST" action="/admin/posts" enctype="multipart/form-data">
                 @csrf
 
                 <div class="mb-6">
@@ -34,6 +37,23 @@
                         type="text">
                     
                     @error('slug')
+                        <p class="text-red-500 text-xs mt-2">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label for="thumbnail" class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                        Thumbnail
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                        name="thumbnail"
+                        id="thumbnail"
+                        type="file">
+                    
+                    @error('thumbnail')
                         <p class="text-red-500 text-xs mt-2">
                             {{ $message }}
                         </p>
