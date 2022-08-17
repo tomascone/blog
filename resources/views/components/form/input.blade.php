dc079362691eee8bbfb4b3c5e273339fa0c75b31
tomascone <tomascone13@gmail.com>
2022-08-17 06:41:03 -0400

    Exteneder admin layout

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,15 +1,15 @@
-@props(['name', 'type' => 'text'])
+@props(['name'])
 
 <div class="mb-6">
     <x-form.label name="{{ $name }}" />
 
     <input 
-        class="border border-gray-400 p-2 w-full" 
+        class="border border-gray-200 p-2 w-full rounded" 
         id="{{ $name }}" 
-        type="{{ $type }}" 
         name="{{ $name }}"
         value="{{ old($name) }}"
         required
+        {{ $attributes }}
     >
     
     <x-form.error name="{{ $name }}" />
